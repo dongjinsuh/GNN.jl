b299d1344b7332dd6ba3bfd7c590a36da0407f16
dongjinsuh <101324577+dongjinsuh@users.noreply.github.com>
2025-06-15 23:23:26 +0200

    Create GNN.jl

---
+++ src/GNN.jl
@@ -0,0 +1,11 @@
+module GNN
+
+using Flux, Graphs, SimpleWeightedGraphs
+using Statistics, GraphNeuralNetworks 
+
+using MLUtils
+using Flux: DataLoader
+using Flux.Losses: logitbinarycrossentropy, binarycrossentropy
+using Flux: @functor
+
+end
